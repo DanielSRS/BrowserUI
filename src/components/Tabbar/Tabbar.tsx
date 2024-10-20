ca03a8d7790da4823c7f7d38f5b092182d031a8f
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2024-10-19 23:33:10 -0300

    ✨ Expand tabbar on hover

--- src/components/Tabbar/Tabbar.tsx
+++ src/components/Tabbar/Tabbar.tsx
@@ -1,10 +1,13 @@
+import { BlurView } from 'blurview';
 import React, {
   forwardRef,
   useImperativeHandle,
+  useMemo,
   useRef,
   useState,
 } from 'react';
 import {
+  Animated,
   FlatList,
   StyleSheet,
   Text,
@@ -21,24 +24,81 @@ const WINDOW_BORDER_SIZE = 6;
 const TITLEBAR_SIZE = 21;
 
 const TABS = [{ id: 13 }, { id: 14 }] as const;
+const TABBAR_EXPANDED_WIDTH = 250;
+const OPEN_ANIMATION_DURATION = 50;
+const CLOSE_ANIMATION_DURATION = 150;
+// const DELAY_TO_EXPAND = 1000;
 
 interface TabbarProps {
   //
 }
 export function Tabbar(props: TabbarProps) {
   const {} = props;
+  const [isHovered, setIsHovered] = useState(false);
+  const tabbarWidth = useRef(new Animated.Value(TABBAR_COLAPSED_WIDTH));
+  const BV = useMemo(() => (isHovered ? BlurView : View), [isHovered]);
+
+  const expand = () => {
+    setIsHovered(true);
+    // Will change fadeAnim value to 1 in 5 seconds
+    Animated.timing(tabbarWidth.current, {
+      toValue: TABBAR_EXPANDED_WIDTH,
+      duration: OPEN_ANIMATION_DURATION,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const colapse = () => {
+    // Will change fadeAnim value to 0 in 3 seconds
+    Animated.timing(tabbarWidth.current, {
+      toValue: TABBAR_COLAPSED_WIDTH,
+      duration: CLOSE_ANIMATION_DURATION,
+      useNativeDriver: false,
+    }).start(() => {
+      setIsHovered(false);
+    });
+  };
 
   return (
-    <View style={sideBar}>
-      <FlatList
-        data={TABS}
-        style={fatlist}
-        contentContainerStyle={fatlistContent}
-        renderItem={({ item }) => <Tab {...item} />}
-      />
+    <View style={tabbarContainer}>
+      {isHovered && <View style={minSideBar} />}
+      <Animated.View
+        // @ts-expect-error
+        onMouseEnter={(_p: MouseEvent) => {
+          expand();
+        }}
+        onMouseLeave={(_p: MouseEvent) => {
+          colapse();
+        }}
+        style={[
+          sideBar,
+          isHovered ? StyleSheet.absoluteFill : undefined,
+          isHovered
+            ? {
+                // backgroundColor: 'rgba(255, 0, 0, 0.5)',
+                maxWidth: TABBAR_EXPANDED_WIDTH,
+                // paddingRight: WINDOW_BORDER_SIZE,
+                width: tabbarWidth.current,
+              }
+            : undefined,
+        ]}>
+        <BV
+          style={[
+            { paddingRight: isHovered ? WINDOW_BORDER_SIZE : undefined },
+            fatlist,
+          ]}>
+          <FlatList
+            data={TABS}
+            contentContainerStyle={fatlistContent}
+            renderItem={({ item }) => <Tab {...item} />}
+          />
+        </BV>
+      </Animated.View>
     </View>
   );
 }
+
+const tabbarContainer = { zIndex: 3 } as const;
 
 const BUTTON_ICON_SIZE = 16;
 const BUTTUN_SIZE = 36;
@@ -61,7 +121,13 @@ const sideBar = {
   paddingTop: 0,
   // backgroundColor: 'blue',
   maxWidth: BUTTUN_SIZE,
+  minWidth: BUTTUN_SIZE,
+  flex: 1,
   // maxWidth: 50,
+} as const;
+
+const minSideBar = {
+  minWidth: BUTTUN_SIZE,
 } as const;
 
 const icon = {
@@ -79,18 +145,26 @@ const TABLIST_GAP = 3;
 const fatlistContent = {
   rowGap: TABLIST_GAP,
 } as const;
+/**
+ * When this value is used, sidebar is absolute positioned, so
+ * its needed compensate right padding not being aplied with
+ * WINDOW_BORDER_SIZE
+ */
+const TABBAR_COLAPSED_WIDTH = BUTTUN_SIZE + WINDOW_BORDER_SIZE;
 
 interface TabProps {
   id: number;
+  onPress?: () => void;
 }
 const Tab = (props: TabProps) => {
-  const { id } = props;
+  const { id, onPress } = props;
   const hoverRef = useRef<HoverViewRef>(null);
 
   return (
     <View key={id}>
       <HoverView ref={hoverRef} style={{ borderRadius: btn.borderRadius }} />
       <TouchableOpacity
+        onPress={onPress}
         onPressIn={() => {
           hoverRef.current?.setOpacity(0.4);
         }}
