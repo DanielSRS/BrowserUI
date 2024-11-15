d88553ec7764c1bc26f79f26aec2d00f15a21e01
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2024-11-14 23:45:12 -0300

    🪲 Fix tab icon color

--- src/components/Tabbar/Tabbar.tsx
+++ src/components/Tabbar/Tabbar.tsx
@@ -182,12 +182,8 @@ const Tab = (props: TabProps) => {
         }}
         style={btn}>
         <View style={btnIconContainer}>
-          <View
-            style={[
-              icon,
-              { borderColor: colors.strokeColorControlStrongStrokeDefault },
-            ]}>
-            <TabDesktopNewPageRegular />
+          <View style={[icon]}>
+            <TabDesktopNewPageRegular color={colors.fillColorTextSecondary} />
           </View>
         </View>
         <Caption
@@ -248,7 +244,7 @@ function TabDesktopNewPageRegular(props: ComponentProps<typeof Svg>) {
       {...props}>
       <Path
         d="M7 12a1 1 0 100-2 1 1 0 000 2zm4-1a1 1 0 11-2 0 1 1 0 012 0zm2 1a1 1 0 100-2 1 1 0 000 2zM3 5.5A2.5 2.5 0 015.5 3h9A2.5 2.5 0 0117 5.5v9a2.5 2.5 0 01-2.5 2.5h-9A2.5 2.5 0 013 14.5v-9zM5.5 4A1.5 1.5 0 004 5.5v9A1.5 1.5 0 005.5 16h9a1.5 1.5 0 001.5-1.5V7H9.5A1.5 1.5 0 018 5.5V4H5.5zM16 5.5A1.5 1.5 0 0014.5 4H9v1.5a.5.5 0 00.5.5H16v-.5z"
-        fill="#212121"
+        fill={props.color ?? '#212121'}
       />
     </Svg>
   );
