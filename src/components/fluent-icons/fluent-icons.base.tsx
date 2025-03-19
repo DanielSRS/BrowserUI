5adb743a17a0babefda4964a8b84b5cd959c0747
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2025-03-19 00:27:42 -0300

    ✨ Add more icons

--- src/components/fluent-icons/fluent-icons.base.tsx
+++ src/components/fluent-icons/fluent-icons.base.tsx
@@ -3,9 +3,9 @@ import type { ComponentProps } from 'react';
 import Svg, { Path } from 'react-native-svg';
 
 export const iconBuilder =
-  (d: string) => (props: ComponentProps<typeof Svg>) => {
-    const width = props.width ?? 20;
-    const height = props.height ?? 20;
+  (d: string) => (props?: ComponentProps<typeof Svg>) => {
+    const width = props?.width ?? 20;
+    const height = props?.height ?? 20;
     return (
       <Svg
         width={width}
@@ -14,7 +14,7 @@ export const iconBuilder =
         fill="none"
         // xmlns="http://www.w3.org/2000/svg"
         {...props}>
-        <Path d={d} fill={props.color ?? '#212121'} />
+        <Path d={d} fill={props?.color ?? '#212121'} />
       </Svg>
     );
   };
