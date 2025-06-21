d99b857a6dded55b733cb5112aceaa181d0cad29
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2025-06-21 07:28:22 -0300

    ⬆️ Upgrade to react-native-windows 0.78 on fabric

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,14 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 const withStorybook = require('@storybook/react-native/metro/withStorybook');
 
+const fs = require('fs');
+const path = require('path');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+
+const rnwPath = fs.realpathSync(
+  path.resolve(require.resolve('react-native-windows/package.json'), '..'),
+);
+
 const defaultConfig = getDefaultConfig(__dirname);
 
 /**
@@ -9,7 +17,30 @@ const defaultConfig = getDefaultConfig(__dirname);
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  //
+  resolver: {
+    blockList: exclusionList([
+      // This stops "npx @react-native-community/cli run-windows" from causing the metro server to crash if its already running
+      new RegExp(
+        `${path.resolve(__dirname, 'windows').replace(/[/\\]/g, '/')}.*`,
+      ),
+      // This prevents "npx @react-native-community/cli run-windows" from hitting: EBUSY: resource busy or locked, open msbuild.ProjectImports.zip or other files produced by msbuild
+      new RegExp(`${rnwPath}/build/.*`),
+      new RegExp(`${rnwPath}/target/.*`),
+      /.*\.ProjectImports\.zip/,
+    ]),
+    //
+  },
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+};
 
 const finalConfig = mergeConfig(defaultConfig, config);
 
---
+++ react-native.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  dependencies: {
+    'react-native-screens': {
+      platforms: {
+        windows: null,
+      },
+    },
+    '@react-native-community/datetimepicker': {
+      platforms: {
+        windows: null,
+      },
+    },
+    '@react-native-community/slider': {
+      platforms: {
+        windows: null,
+      },
+    },
+  },
+};
