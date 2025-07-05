a218bcb9a8c8c7854cc1f10fb0783a48042cb30e
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2025-07-05 05:28:55 -0300

    📝 Add DownloadProgress component stories

---
+++ src/components/download-progress/download-progress.stories.tsx
@@ -0,0 +1,23 @@
+import { DownloadProgress } from './download-progress';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'Components/DownloadProgress',
+  component: DownloadProgress,
+  argTypes: {},
+  args: {
+    donwloadId: 324,
+    downloadedSize: 1024 * 1024 * 10, // 10 MB
+    totalSize: 1024 * 1024 * 100, // 100 MB
+    downloadSpeed: 1024 * 1024, // 1 MB/s
+    fileName: 'example-file.zip',
+    remainingTime: 30, // seconds
+    status: 'downloading',
+  },
+} satisfies Meta<typeof DownloadProgress>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Basic: Story = {};
