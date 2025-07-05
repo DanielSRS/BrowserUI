65fc1e2bf58732cbef0f5c29578c7832fa2a77ab
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2025-07-04 23:56:23 -0300

    🪲 Use storybook lite ui on windows

--- .rnstorybook/index.tsx
+++ .rnstorybook/index.tsx
@@ -1,11 +1,15 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Constants } from '@danielsrs/react-native-sdk';
 import { view } from './storybook.requires';
+import { LiteUI } from '@storybook/react-native-ui-lite';
 
 const StorybookUIRoot = view.getStorybookUI({
   storage: {
     getItem: AsyncStorage.getItem,
     setItem: AsyncStorage.setItem,
   },
+  // @ts-expect-error
+  CustomUIComponent: Constants.IS_WINDOWS ? LiteUI : undefined,
 });
 
 export default StorybookUIRoot;
