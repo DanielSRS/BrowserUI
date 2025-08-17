e67cada0178661fdc140782ae90c9e4ceae0efb1
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2025-08-16 22:03:16 -0300

    ✨ Add user creation page

---
+++ src/Pages/user-creation/user-creation.stories.tsx
@@ -0,0 +1,15 @@
+import { fn } from 'storybook/test';
+import { UserCreation } from './user-creation';
+import type { Meta, StoryObj } from '@storybook/react-native';
+
+const meta = {
+  title: 'Pages/UserCreation',
+  component: UserCreation,
+  tags: ['autodocs'],
+  args: { onClick: fn() },
+} satisfies Meta<typeof UserCreation>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {};
