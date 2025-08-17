f36d59c8dd3d1473231096d6dfd552bbfab7b240
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2025-08-16 22:59:56 -0300

    ✨ Add function to create and registry a new user

--- src/data/user.ts
+++ src/data/user.ts
@@ -3,6 +3,8 @@ import { syncPlugin } from './sync/sync-pluggin';
 import type { User } from '../../types/user';
 import type { ObservableSyncState } from '@legendapp/state';
 
+const CURRENT_USER_TYPE_VERSION = 1;
+
 type UserId = number;
 
 interface UserRegistry {
@@ -22,9 +24,13 @@ interface UserRegistry {
    * Whether there are any users in the registry.
    */
   hasAnyUser: boolean;
+  /**
+   * Creates a new user and adds them to the registry.
+   */
+  createUser: (name: string) => User;
 }
 
-const allUsers$ = observable(
+const allUsers$ = observable<Record<UserId, User>>(
   syncPlugin({
     initial: {},
   }),
@@ -35,11 +41,23 @@ const usersCount$ = observable(() => {
   return Object.keys(allUsers$.get(true)).length;
 });
 
-export const user$ = observable<UserRegistry>({
+export const users$ = observable<UserRegistry>({
   all: allUsers$,
   count: usersCount$,
   syncState: usersSyncState$,
   hasAnyUser: () => {
     return usersSyncState$.isPersistLoaded.get() && usersCount$.get() > 0;
+  },
+  createUser: (name: string) => {
+    const now = Date.now();
+    const newUser: User = {
+      id: now,
+      name,
+      ' createdAt': now,
+      ' updatedAt': now,
+      ' version': CURRENT_USER_TYPE_VERSION,
+    };
+    allUsers$[newUser.id].set(newUser);
+    return newUser;
   },
 });
